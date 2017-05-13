fe97a2f30b7bcc81fd8e8d006885910d0166b867
jmjoy <918734043@qq.com>
2017-05-13 14:30:58 +0800

    Basic framework.

---
+++ src/Expression.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace arSql;
+
+class Expression {
+
+    /**
+     * @var string the DB expression
+     */
+    public $expression;
+    /**
+     * @var array list of parameters that should be bound for this expression.
+     * The keys are placeholders appearing in [[expression]] and the values
+     * are the corresponding parameter values.
+     */
+    public $params = array();
+
+    /**
+     * Constructor.
+     * @param string $expression the DB expression
+     * @param array $params parameters
+     */
+    public function __construct($expression, $params = array())
+    {
+        $this->expression = $expression;
+        $this->params = $params;
+    }
+
+    /**
+     * String magic method
+     * @return string the DB expression
+     */
+    public function __toString()
+    {
+        return $this->expression;
+    }
+
+}
